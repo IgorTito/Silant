83559ae68c16304ab2e65980b0ba994b3df92359
IgorTito <igortito@inbox.ru>
2023-02-06 19:09:49 +0300

    change models

---
+++ Backend/mysilantapp/views.py
@@ -0,0 +1,214 @@
+from django.db.models import Q
+from rest_framework import filters
+from rest_framework.generics import ListAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView, RetrieveAPIView
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny, IsAuthenticatedOrReadOnly
+from rest_framework.authentication import TokenAuthentication
+from . import serializers
+from .filters import MachineFilter, MaintenanceFilter, ClaimFilter
+from .models import *
+from .serializers import MaintenanceSerializer, ClaimSerializer
+
+
+class UserCroupView(ListAPIView):
+    serializer_class = serializers.UserSilantSerializer
+    queryset = UserSilant.objects.all()
+    permission_classes = (AllowAny,)
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['silUser__username']
+
+
+class MachineAPIViewPublic(ListAPIView):
+    serializer_class = serializers.MachinePublicSerializer
+    queryset = Machine.objects.all()
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['serNumM']
+    permission_classes = (AllowAny,)
+
+
+class MachineAPIView(ListCreateAPIView):
+    serializer_class = serializers.MachineSerializer
+    queryset = Machine.objects.all()
+    permission_classes = (AllowAny,)
+    authentication_classes = (TokenAuthentication,)
+    filterset_class = MachineFilter
+    ordering = ['dateShipmentFactory']
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filter'] = MachineFilter(self.request.GET, queryset=self.get_queryset())
+        return context
+
+    def get_queryset(self):
+        user = self.request.user
+        if UserSilant.objects.get(silUser=user).categoryType == "MG":
+            return Machine.objects.all()
+        if UserSilant.objects.get(silUser=user).categoryType == "CL":
+            return Machine.objects.filter(userClient=UserSilant.objects.get(silUser=user))
+        if UserSilant.objects.get(silUser=user).categoryType == "SK":
+            return Machine.objects.filter(userService=ServiceCompany.objects.get(user=UserSilant.objects.get
+                                                                                       (silUser=user)))
+
+
+class MachineAPIViewDetail(RetrieveUpdateDestroyAPIView):
+    serializer_class = serializers.MachineSerializer
+    queryset = Machine.objects.all()
+    permission_classes = (AllowAny,)
+    authentication_classes = (TokenAuthentication,)
+
+
+class MaintenanceAPIView(ListCreateAPIView):
+    serializer_class = serializers.MaintenanceSerializer
+    queryset = Maintenance.objects.all()
+    permission_classes = (AllowAny,)
+    authentication_classes = (TokenAuthentication,)
+    filterset_class = MaintenanceFilter
+    ordering = ['date']
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filter'] = MaintenanceFilter(self.request.GET, queryset=self.get_queryset())
+        return context
+
+    def get_queryset(self):
+        user = self.request.user
+        if UserSilant.objects.get(silUser=user).categoryType == "MG":
+            return Maintenance.objects.all()
+        if UserSilant.objects.get(silUser=user).categoryType == "CL":
+            return Maintenance.objects.filter(
+                machine=Machine.objects.filter(userClient=UserSilant.objects.get(silUser=user)))
+        if UserSilant.objects.get(silUser=user).categoryType == "SK":
+            return Maintenance.objects.filter(machine=Machine.objects.filter(
+                userService=ServiceCompany.objects.get(user=UserSilant.objects.get(silUser=user))))
+
+
+class MaintenanceAPIViewDetail(RetrieveUpdateDestroyAPIView):
+    serializer_class = MaintenanceSerializer
+    queryset = Maintenance.objects.all()
+    permission_classes = (AllowAny,)
+    authentication_classes = (TokenAuthentication,)
+
+
+class ClaimAPIView(ListCreateAPIView):
+    serializer_class = ClaimSerializer
+    queryset = Claim.objects.all()
+    permission_classes = (AllowAny,)
+    authentication_classes = (TokenAuthentication,)
+    filterset_class = ClaimFilter
+    search_fields = ['dateRejection', 'operTime', 'typefailure_title', 'failDescription', 'recmethode_title',
+                     'spareParts', 'dateRecovery', 'downtime', 'machine_title']
+    ordering = ['dateRejection']
+
+    def get_queryset(self):
+        user = self.request.user
+        if UserSilant.objects.get(silUser=user).categoryType == "MG":
+            return Claim.objects.all()
+        if UserSilant.objects.get(silUser=user).categoryType == "CL":
+            return Claim.objects.filter(machine=Machine.objects.filter(userClient=UserSilant.objects.get(silUser=user)))
+        if UserSilant.objects.get(silUser=user).categoryType == "SK":
+            return Claim.objects.filter(machine=Machine.objects.filter(
+                userService=ServiceCompany.objects.get(user=UserSilant.objects.get(silUser=user))))
+
+
+class ClaimAPIViewDetail(RetrieveUpdateDestroyAPIView):
+    serializer_class = ClaimSerializer
+    queryset = Claim.objects.all()
+    permission_classes = (AllowAny,)
+    authentication_classes = (TokenAuthentication,)
+
+
+class ReferenceBookAPIView:
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = (TokenAuthentication,)
+
+    class Meta:
+        abstract = True
+
+
+class ModelMachineAPIView(ListCreateAPIView):
+    serializer_class = serializers.ModelMachineSerializer
+    queryset = ModelMachine.objects.all()
+
+
+class ModelMachineDetailAPIView(RetrieveUpdateDestroyAPIView):
+    serializer_class = serializers.ModelMachineSerializer
+    queryset = ModelMachine.objects.all()
+
+
+class ModelEngineAPIView(ListCreateAPIView):
+    serializer_class = serializers.ModelEngineSerializer
+    queryset = ModelEngine.objects.all()
+
+
+class ModelEngineDetailAPIView(RetrieveUpdateDestroyAPIView):
+    serializer_class = serializers.ModelEngineSerializer
+    queryset = ModelEngine.objects.all()
+
+
+class ModelTransmissionAPIView(ListCreateAPIView):
+    serializer_class = serializers.ModelTransmissionSerializer
+    queryset = ModelTransmission.objects.all()
+
+
+class ModelTransmissionDetailAPIView(RetrieveUpdateDestroyAPIView):
+    serializer_class = serializers.ModelTransmissionSerializer
+    queryset = ModelTransmission.objects.all()
+
+
+class ModelDriveAxleAPIView(ListCreateAPIView):
+    serializer_class = serializers.ModelDriveAxleSerializer
+    queryset = ModelDriveAxle.objects.all()
+
+
+class ModelDriveAxleDetailAPIView(RetrieveUpdateDestroyAPIView):
+    serializer_class = serializers.ModelDriveAxleSerializer
+    queryset = ModelDriveAxle.objects.all()
+
+
+class ModelSteeringAxleAPIView(ListCreateAPIView):
+    serializer_class = serializers.ModelSteeringAxleSerializer
+    queryset = ModelSteeringAxle.objects.all()
+
+
+class ModelSteeringAxleDetailAPIView(RetrieveUpdateDestroyAPIView):
+    serializer_class = serializers.ModelSteeringAxleSerializer
+    queryset = ModelSteeringAxle.objects.all()
+
+
+class ServiceCompanyAPIView(ListCreateAPIView):
+    serializer_class = serializers.ServiceCompanySerializer
+    queryset = ServiceCompany.objects.all()
+
+
+class ServiceCompanyDetailAPIView(RetrieveUpdateDestroyAPIView):
+    serializer_class = serializers.ServiceCompanySerializer
+    queryset = ServiceCompany.objects.all()
+
+
+class TypeMaintenanceAPIView(ListCreateAPIView):
+    serializer_class = serializers.TypeMaintenanceSerializer
+    queryset = TypeMaintenance.objects.all()
+
+
+class TypeMaintenanceDetailAPIView(RetrieveUpdateDestroyAPIView):
+    serializer_class = serializers.TypeMaintenanceSerializer
+    queryset = TypeMaintenance.objects.all()
+
+
+class TypeFailureAPIView(ListCreateAPIView):
+    serializer_class = serializers.TypeFailureSerializer
+    queryset = TypeFailure.objects.all()
+
+
+class TypeFailureDetailAPIView(RetrieveUpdateDestroyAPIView):
+    serializer_class = serializers.TypeFailureSerializer
+    queryset = TypeFailure.objects.all()
+
+
+class RecoveryMethodAPIView(ListCreateAPIView):
+    serializer_class = serializers.RecoveryMethodSerializer
+    queryset = RecoveryMethod.objects.all()
+
+
+class RecoveryMethodDetailAPIView(RetrieveUpdateDestroyAPIView):
+    serializer_class = serializers.RecoveryMethodSerializer
+    queryset = RecoveryMethod.objects.all()
